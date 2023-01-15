524ae33dfae704992ffe1d64cc9154416e1db265
Ben Schuman <ben@jeffschuman.com>
2023-01-15 15:47:42 -0500

    initial commit

---
+++ app/api/solver.py
@@ -0,0 +1,48 @@
+from flask import request, jsonify
+from flask_cors import cross_origin
+from . import api
+import pickle
+import os
+
+def prefix_contains(prefix_str, word_dictionary):
+    for word in word_dictionary:
+        loc = word.index(prefix_str) if prefix_str in word else -1
+        if loc == 0:
+            return True
+    return False
+    
+    
+
+def find_words(word, word_dictionary, letters, gold_letter, result_list):
+    # base case 1: we find a word in the dictionary that contains the gold letter
+    # we've already pruned words < length of 4 so don't have to check word length
+    if word in word_dictionary and gold_letter in word:
+        result_list.append(word)
+    
+    if prefix_contains(word, word_dictionary):
+        for letter in letters:
+            find_words(word + letter, word_dictionary, letters, gold_letter, result_list)
+        
+    else:
+        return
+
+@api.route('/solve', methods=['POST', 'OPTIONS'])
+@cross_origin()
+def solve():
+    req_body = request.json
+    
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    
+    pkl_url = os.path.join(SITE_ROOT, 'static', 'dictionary.pkl')
+    
+    with open(pkl_url, "rb") as dict_file:
+        word_dictionary = pickle.load(dict_file)
+            
+    letters = list(req_body['letters'])
+    gold_letter = req_body['gold_letter']
+    
+    words = []
+    
+    find_words("", word_dictionary, letters, gold_letter, result_list=words)
+    
+    return jsonify(words)
